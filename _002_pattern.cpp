d6269da4ac975cfaef13527b97229d86a0197d46
SinhaAmrit <purpleplots@gmail.com>
2021-11-29 08:52:44 +0530

    _002_pattern.cpp Added

--- _002_pattern.cpp
+++ _002_pattern.cpp
@@ -1,21 +1,21 @@
 /*  Sample Output 👇
-    * * * * * 
-    * * * * * 
-    * * * * * 
-    * * * * * 
-    * * * * * 
+    1 1 1 1 1 
+    2 2 2 2 2
+    3 3 3 3 3 
+    4 4 4 4 4 
+    5 5 5 5 5  
 */
 #include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout << "Enter the size of square matrix:";
+    cout << "Enter the size: ";
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
-            cout << "* ";
+            cout << i << ends;
         cout << endl;
     }
 }
