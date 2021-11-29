4f907c73fd9166865c9d66040b574c2a8d4d37e4
Amrit Sinha <purpleplots@gmail.com>
2021-11-29 11:53:32 +0530

    _006_pattern Added

---
+++ _006_pattern.cpp
@@ -0,0 +1,22 @@
+/*  Sample Output 👇
+    1 
+    1 2 
+    1 2 3 
+    1 2 3 4 
+    1 2 3 4 5 
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter Size: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        int count = 1;
+        for (int j = 1; j <= i; j++)
+            cout << count++;
+        cout << endl;
+    }
+}
