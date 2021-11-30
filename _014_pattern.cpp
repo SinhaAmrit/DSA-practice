d3a427f0fe430f108c21083363e2a3860b02a03f
Amrit Sinha <purpleplots@gmail.com>
2021-11-30 08:23:32 +0530

    _014_pattern.cpp Added

--- _014_pattern.cpp
+++ _014_pattern.cpp
@@ -1,6 +1,6 @@
 /*  Sample Output 👇
-    A B C 
-    B C D  
+    A * * 
+    B C *  
     C D E 
 */
 #include <iostream>
@@ -12,11 +12,13 @@ int main()
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= i; j++)
         {
             char ch = 'A' + i + j - 2;
             cout << ch << " ";
         }
+        for (int j = n - i; j >= 1; j--)
+            cout << "* ";
         cout << endl;
     }
 }
