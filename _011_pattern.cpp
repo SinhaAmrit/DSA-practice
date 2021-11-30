95f59b13535b7e39bd3a186fdec2a240c83dc2e5
Amrit Sinha <purpleplots@gmail.com>
2021-11-30 07:32:25 +0530

    _011_pattern.cpp Added

---
+++ _011_pattern.cpp
@@ -0,0 +1,23 @@
+/*  Sample Output 👇
+    A A A A
+    B B B B 
+    C C C C 
+    D D D D
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            char c = 'A' + j - 1;
+            cout << c << " ";
+        }
+        cout << endl;
+    }
+}
