5d17a19cc5626669b75c7cde427d9637476b2757
Amrit Sinha <purpleplots@gmail.com>
2021-11-30 07:51:42 +0530

    _012_pattern.cpp Modifieded

--- _012_pattern.cpp
+++ _012_pattern.cpp
@@ -7,17 +7,28 @@
 using namespace std;
 int main()
 {
-    int n, count;
+    // Method = 1;
+    // int n, count = 1;
+    // cout << "Enter size: ";
+    // cin >> n;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         char c = 'A' + count++ - 1;
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // Method = 2;
+    int n;
+    char value = 'A';
     cout << "Enter size: ";
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        count = 1;
         for (int j = 1; j <= n; j++)
-        {
-            char c = 'A' + count++ - 1;
-            cout << c << " ";
-        }
+            cout << value++ << " ";
         cout << endl;
     }
 }
