b695455133c92372f38e83273baec0c32be3e6d1
Amrit Sinha <purpleplots@gmail.com>
2021-12-03 08:41:19 +0530

    _026_power-of-two.cpp Optimised

--- _026_power-of-two.cpp
+++ _026_power-of-two.cpp
@@ -2,13 +2,23 @@
 using namespace std;
 int main()
 {
-    int n;
     bool isPowerOfTwo = false;
+    // int n;
+    // cout << "Enter an integer to check: " << ends;
+    // cin >> n;
+    // for (int i = 0; i < 30; i++)
+    //     if (pow(2, i) == n)
+    //         isPowerOfTwo = true;
+    //optimised Solution Starts
+    int n, ans = 1;
     cout << "Enter an integer to check: " << ends;
     cin >> n;
-    for (int i = 0; i < 30; i++)
-        if (pow(2, i) == n)
+    for (int i = 0; i <= 30; i++)
+        if (ans == n)
             isPowerOfTwo = true;
+        else if (ans < INT_MAX / 2 || ans * 2 > INT_MAX)
+            ans *= 2;
+    //optimised Solution Ends
     if (isPowerOfTwo)
         cout << n << " is in power of Two" << endl;
     else
